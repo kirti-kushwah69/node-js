cfd84ddd58b764ec288ae562958aeff7d5185e5e
kirti-kushwah69 <kirtikushwah906@gmail.com>
2022-12-28 19:25:28 +0530

    node js

---
+++ module.js
@@ -0,0 +1,4 @@
+const data = require('./student');
+console.log(data.name);
+
+data.addition(2,3);
---
+++ student.js
@@ -0,0 +1,21 @@
+// console.log("kirti");
+// console.log("i am student")
+
+// const age = 20;
+// const college = "itm";
+
+// module.exports.a = age;
+// module.exports.b = college;
+
+module.exports ={
+    name:'ram',
+    age:23,
+    city:'goa',
+    college:'itm',
+    addition:function(a,b){
+        console.log("its a function",a+b);
+    },
+    substraction:function(){
+        console.log("its a substraction");
+    }
+}
